064ef3dd9c818f5796c9715401ec1f8fa6d80d72
Lucas Reis <lucasreis_9@hotmail.com>
2023-07-18 18:25:52 -0300

    teste com Future.delay e com onPlayerComplete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-    
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme:
+            GoogleFonts.plusJakartaSansTextTheme(Theme.of(context).textTheme),
       ),
-      home: ,
+      home: HomePage(),
     );
   }
 }
